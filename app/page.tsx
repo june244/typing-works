a5dac6f97955b65a492592f5144fdab9bf493533
june244 <kyjune9993@gmail.com>
2024-11-17 22:38:06 +0900

    title set

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 '>
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start w-[960px]'>
         <div className='p-3 mb-4 text-3xl m-auto'>
-          헤림이 전용 타이핑 연습장!
+          😍📄 혜림이 전용 타이핑 연습장! ⌨️😍
         </div>
         <TypingProvider>
           <TypingTest />
